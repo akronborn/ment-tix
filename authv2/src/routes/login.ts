f800b06a5b9bf66c10a0c2c8800b98ead60c9716
Clotia Johari <freewillandjustice@gmail.com>
2021-07-01 09:27:30 -0400

    added request validation middleware

--- authv2/src/routes/login.ts
+++ authv2/src/routes/login.ts
@@ -1,9 +1,17 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middleware/validate-request';
 
 const router = express.Router();
 
-router.post('/api/users/login', (req, res) => {
-  res.send('Hello from login');
-});
+router.post(
+  '/api/users/login',
+  [
+    body('email').isEmail().withMessage('Must input valid email address'),
+    body('password').trim().notEmpty().withMessage('Email required'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {}
+);
 
 export { router as loginRouter };
