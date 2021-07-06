c861b3f5448629b0fe43a0362a5ce3d1ddd1b88b
Clotia Johari <freewillandjustice@gmail.com>
2021-07-06 01:25:09 -0400

    added testing packages, updated docker file

--- authv2/src/index.ts
+++ authv2/src/index.ts
@@ -1,36 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
+import { app } from './app';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { activeUserRouter } from './routes/active-user';
-import { signupRouter } from './routes/signup';
-import { loginRouter } from './routes/login';
-import { logoutRouter } from './routes/logout';
-import { errorHandler } from './middleware/error-handler';
-import { PageNotFound } from './errors/page-not-found-error';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(activeUserRouter);
-app.use(signupRouter);
-app.use(loginRouter);
-app.use(logoutRouter);
-
-app.all('*', async (req, res) => {
-  throw new PageNotFound();
-});
-
-app.use(errorHandler);
 
 const startDB = async () => {
   if (!process.env.JWT_KEY) {
--- authv2/src/routes/active-user.ts
+++ authv2/src/routes/active-user.ts
@@ -1,13 +1,9 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
-import { UnAuthorizedError } from '../errors/unauthorized-error';
-
 import { activeUser } from '../middleware/active-user-state';
-import { authWall } from '../middleware/auth-wall';
 
 const router = express.Router();
 
-router.get('/api/users/activeuser', activeUser, authWall, (req, res) => {
+router.get('/api/users/activeuser', activeUser, (req, res) => {
   res.send({ activeUser: req.activeUser || null });
 });
 
