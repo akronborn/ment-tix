19a6117d2f9c8dd2520bc648d53c6d5667ec2758
Clotia Johari <freewillandjustice@gmail.com>
2021-07-31 01:43:55 -0400

    passed all tests, updated 'new' route

--- tix/src/routes/__test__/new.test.ts
+++ tix/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Tix } from '../../models/tix';
 
 it('has post request route handler listening', async () => {
   const response = await request(app).post('/api/tix').send({});
@@ -26,6 +27,7 @@ it('error thrown when title is invalid ', async () => {
     .set('Cookie', global.signin())
     .send({
       title: '',
+      body: 'description',
       price: 10,
     })
     .expect(400);
@@ -34,6 +36,7 @@ it('error thrown when title is invalid ', async () => {
     .post('/api/tix')
     .set('Cookie', global.signin())
     .send({
+      body: 'description',
       price: 10,
     })
     .expect(400);
@@ -45,6 +48,7 @@ it('error thrown when price is invalid ', async () => {
     .set('Cookie', global.signin())
     .send({
       title: 'Sample Title',
+      body: 'description',
     })
     .expect(400);
 
@@ -53,18 +57,26 @@ it('error thrown when price is invalid ', async () => {
     .set('Cookie', global.signin())
     .send({
       title: 'Sample Title',
+      body: 'sample body',
       price: -1,
     })
     .expect(400);
 });
 
 it('ticket created when all inputs are valid ', async () => {
+  let tix = await Tix.find({});
+  expect(tix.length).toEqual(0);
+
   await request(app)
     .post('/api/tix')
+    .set('Cookie', global.signin())
     .send({
       title: 'Sample Title 2',
+      body: 'Description',
       price: 20.0,
     })
-
     .expect(201);
+
+  tix = await Tix.find({});
+  expect(tix.length).toEqual(1);
 });
