4958ea12a9bede7a67b374d142e32fa437a71975
Clotia Johari <freewillandjustice@gmail.com>
2021-08-19 23:32:53 -0400

    added tests and completed order routes

--- orders/src/models/tix.ts
+++ orders/src/models/tix.ts
@@ -1,13 +1,18 @@
 import mongoose from 'mongoose';
+import { Order } from './order';
+import { OrderStatus } from '../middleware/states/order-status';
 
 interface TixAttrs {
   title: string;
+  content: string;
   price: number;
 }
 
 export interface TixDoc extends mongoose.Document {
   title: string;
+  content: string;
   price: number;
+  isReserved(): Promise<boolean>;
 }
 
 interface TixModel extends mongoose.Model<TixDoc> {
@@ -42,6 +47,21 @@ const tixSchema = new mongoose.Schema(
 
 tixSchema.statics.build = (attrs: TixAttrs) => {
   return new Tix(attrs);
+};
+
+tixSchema.methods.isReserved = async function () {
+  const existingOrder = await Order.findOne({
+    tix: this as any,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.PendingPayment,
+        OrderStatus.Complete,
+      ],
+    },
+  });
+
+  return !!existingOrder;
 };
 
 const Tix = mongoose.model<TixDoc, TixModel>('Tix', tixSchema);
