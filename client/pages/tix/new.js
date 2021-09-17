993f974dc5a64a1bc610eb195ab0ddda9ed765c1
Clotia Johari <freewillandjustice@gmail.com>
2021-09-17 19:17:52 -0400

    added length check to validation and logic

--- client/pages/tix/new.js
+++ client/pages/tix/new.js
@@ -1,11 +1,48 @@
+import { useState } from 'react';
+import useRequest from '../.././hooks/use-request';
+import Router from 'next/router';
+
 const NewTix = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [price, setPrice] = useState('');
+  const { makeRequest, errors } = useRequest({
+    url: '/api/tix',
+    method: 'post',
+    body: {
+      title,
+      content,
+      price,
+    },
+    // change to /tix/:tixId
+    onSuccess: () => Router.push('/'),
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    makeRequest();
+  };
+
+  const onBlur = () => {
+    const value = parseFloat(price);
+
+    if (isNaN(value)) {
+      return;
+    }
+
+    setPrice(value.toFixed(2));
+  };
+
   return (
     <div>
       <h1>Create Tix</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label>Title</label>
           <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             placeholder={'Ex: Sr. Developer Q&A for Jr. Developers'}
             className="form-control"
           />
@@ -13,6 +50,8 @@ const NewTix = () => {
         <div className="form-group">
           <label>Content</label>
           <input
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
             placeholder={'Provide a description and availability'}
             className="form-control"
           />
@@ -20,10 +59,14 @@ const NewTix = () => {
         <div className="form-group">
           <label>Price</label>
           <input
+            value={price}
+            onBlur={onBlur}
+            onChange={(e) => setPrice(e.target.value)}
             placeholder={'Price must be a number, no symbols'}
             className="form-control"
           />
         </div>
+        {errors}
         <button className="btn btn-primary">Submit</button>
       </form>
     </div>
